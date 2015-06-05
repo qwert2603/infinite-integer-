47ecf181f5d18e6741c21d7600da002c46f38a93
Alex Zhdanov <qwert2603@mail.ru>
2015-06-05 15:27:50 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
 	typedef infinite_integer::Digit D; 
-	D d1 = "349657539278576539476405850968365684535645346785646453";
+	D d1 = "-349657539278576539476405850968365684535645346785646453";
 	D d2 = 91345747845601;
 	cout << d1 + d2 << '\n' << d1 - d2 << endl;
 	cout << d1 / d2 << '\n' << d1 % d2 << endl;
@@ -14,6 +14,7 @@ int main() {
 	cout << d3 << endl;
 	d3 /= string("10000000000000000000000000000000000000000000000");
 	cout << d3 << endl;
+	cout << (d1 == d2);
 	
 	return 0;
 }
